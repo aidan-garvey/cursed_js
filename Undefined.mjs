bfdaefdacda20cc50325a1b59a1c4d0baf4b85f6
Aidan Garvey <aidangarvey@pm.me>
2026-03-06 18:53:32 -0600

    Improve Undefined example

--- Undefined.mjs
+++ Undefined.mjs
@@ -1,8 +1,12 @@
 "use strict";
 
 function foo() {
-    const undefined = 0;
-    console.log(typeof undefined);
+  const undefined = 0;
+  console.log(typeof undefined); // number
+  console.log(typeof globalThis.undefined); // undefined
 }
 
-foo(); // number
+foo();
+
+console.log("undefined" in globalThis); // true
+console.log(globalThis.undefined === undefined); // true
