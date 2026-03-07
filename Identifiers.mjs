5521ecdf115823d8a60ab07c07e65d1842c796f7
Aidan Garvey <aidangarvey@pm.me>
2026-03-06 18:54:50 -0600

    Add examples of wacky identifiers

---
+++ Identifiers.mjs
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(typeof undeclaredVariable); // undefined
+try {
+  console.log(undeclaredVariable);
+}
+catch (err) {
+  console.log(err instanceof ReferenceError); // true
+  console.error(err.message); // undeclaredVariable is not defined
+}
+
+const variabl\u{65} = 1
+console.log(variable); // 1
+
+const \u0068\u0065\u006C\u006C\u006F = 2;
+console.log(hello); // 2
