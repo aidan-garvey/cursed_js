81eb4a28bbae74d4d6a1789ef75df330e3e49a52
Aidan Garvey <aidangarvey@pm.me>
2026-03-11 20:09:07 -0500

    Improve GetterImplements example

--- src/GetterImplements.ts
+++ src/GetterImplements.ts
@@ -7,16 +7,20 @@ interface IExample {
 }
 
 // Maybe you don't want to modify the interface because you're implementing a
-// mock class for unit tests, for example.
+// mock class for unit tests, or trying to debug something.
 class ExampleMock implements IExample {
   private _field = 0
+  private readCount = 0
+  private writtenValues: number[] = []
 
   get field(): number {
+    ++this.readCount
     return this._field
   }
   // (fun fact: it's an error in TS to annotate the return type here, even if
   // it's void)
   set field(value: number) {
+    this.writtenValues.push(value)
     this._field = value
   }
 }
